f8514bff2a53dc2b7a241c179c4b19703a55f399
Kirsty Balfour <2635375b@student.gla.ac.uk>
2024-10-12 18:34:35 +0100

    Adding Clothes Images

--- public/js/dress-up.js
+++ public/js/dress-up.js
@@ -4,18 +4,27 @@ const currentDay = 2;
 
 document.getElementById("day-title").textContent = days[currentDay];
 
+tops = ['../imgs/tops/white-shirt.png', '../imgs/tops/black-shirt.png', '../imgs/tops/blue-shirt.png', '../imgs/tops/north-face-hoodie.png'];
+
 // Define an array of items
-const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+const clothesList = tops;
 
 // Select the <ul> element where the items will be added
-const itemsList = document.getElementById('current-images');
+var imgDiv = document.getElementsByClassName('div1')[0];
 
 // Use a for loop to create list items dynamically
-for (let i = 0; i < items.length; i++) {
+for (let i = 0; i < clothesList.length; i++) {
+    imgDiv = document.getElementsByClassName(`div${i+1}`)[0];
+    console.log(i)
     // Create a new <li> element
-    const li = document.createElement('li');
+    const image = document.createElement('img');
     // Set the text content of the <li> to the current item
-    li.textContent = items[i];
+    image.src = clothesList[i];
+    image.className = "clothing-item";
     // Append the <li> to the <ul>
-    itemsList.appendChild(li);
+    imgDiv.appendChild(image);
 }
+
+document.getElementsByClassName('tops')[0].addEventListener('click', function() {
+    console.log('tops');
+});
