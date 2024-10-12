656da7879491e794e9771cea8e49c9cdc1cfbbd4
Kirsty Balfour <2635375b@student.gla.ac.uk>
2024-10-12 19:02:56 +0100

    Switch clothes images.

--- public/js/dress-up.js
+++ public/js/dress-up.js
@@ -1,30 +1,72 @@
 const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
-const currentDay = 2;
+const currentDay = 0;
 
 document.getElementById("day-title").textContent = days[currentDay];
 
 tops = ['../imgs/tops/white-shirt.png', '../imgs/tops/black-shirt.png', '../imgs/tops/blue-shirt.png', '../imgs/tops/north-face-hoodie.png'];
+bottoms = ['../imgs/skirts/blue-skirt.png', '../imgs/skirts/brown-skirt.png', '../imgs/skirts/green-skirt.png', '../imgs/skirts/red-tartan-skirt.png'];
+fullLength = [];
+jackets = [];
+shoes = [];
+hats = [];
 
-// Define an array of items
-const clothesList = tops;
 
-// Select the <ul> element where the items will be added
+var clothesList = tops;
+
 var imgDiv = document.getElementsByClassName('div1')[0];
 
-// Use a for loop to create list items dynamically
-for (let i = 0; i < clothesList.length; i++) {
-    imgDiv = document.getElementsByClassName(`div${i+1}`)[0];
-    console.log(i)
-    // Create a new <li> element
-    const image = document.createElement('img');
-    // Set the text content of the <li> to the current item
-    image.src = clothesList[i];
-    image.className = "clothing-item";
-    // Append the <li> to the <ul>
-    imgDiv.appendChild(image);
+renderClothes();
+
+function renderClothes() {
+    for (let i = 0; i < clothesList.length; i++) {
+        imgDiv = document.getElementsByClassName(`div${i + 1}`)[0];
+
+        while (imgDiv.firstChild) {
+            imgDiv.removeChild(imgDiv.firstChild);
+        }
+
+        console.log(i)
+        const image = document.createElement('img');
+        image.src = clothesList[i];
+        image.className = "clothing-item";
+        imgDiv.appendChild(image);
+    }
 }
 
-document.getElementsByClassName('tops')[0].addEventListener('click', function() {
-    console.log('tops');
+document.getElementsByClassName('tops')[0].addEventListener('click', function () {
+    clothesList = tops;
+    renderClothes();
+    console.log(clothesList)
 });
+
+document.getElementsByClassName('bottoms')[0].addEventListener('click', function () {
+    clothesList = bottoms;
+    renderClothes()
+    console.log(clothesList)
+});
+
+document.getElementsByClassName('full-length')[0].addEventListener('click', function () {
+    clothesList = fullLength;
+    renderClothes()
+    console.log(clothesList)
+});
+
+document.getElementsByClassName('jackets')[0].addEventListener('click', function () {
+    clothesList = jackets;
+    renderClothes()
+    console.log(clothesList)
+});
+
+document.getElementsByClassName('shoes')[0].addEventListener('click', function () {
+    clothesList = shoes;
+    renderClothes()
+    console.log(clothesList)
+});
+
+document.getElementsByClassName('hats')[0].addEventListener('click', function () {
+    clothesList = hats;
+    renderClothes()
+    console.log(clothesList)
+});
+
